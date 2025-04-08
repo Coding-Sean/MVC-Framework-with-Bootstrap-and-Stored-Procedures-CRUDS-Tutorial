d3fcbcafa897bc6f0e82d4741572e39a11d70561
Jansean Libera <liberajansean34@gmail.com>
2025-04-08 20:51:41 +0800

    Add files via upload
    FILES registration

---
+++ registration/view/create.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add User</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <form action="../controller/UserController.php?action=create" method="post" class="container mt-5">
+        <h2>Add User</h2>
+        <div class="mb-3">
+            <label>Lastname:</label>
+            <input type="text" name="lastname" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label>Firstname:</label>
+            <input type="text" name="firstname" class="form-control" required>
+        </div>
+        <button class="btn btn-success">Save</button>
+        <a href="index.php" class="btn btn-secondary">Back</a>
+    </form>
+</body>
+</html>
